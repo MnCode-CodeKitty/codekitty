840875709dc346a74fc678f7c5611d48bc2ca7f2
codekitty-jason <30638615+codekitty-jason@users.noreply.github.com>
2019-01-01 16:21:43 -0600

    Rewrote library and smartavoid code to optimize
    Very large rewrite of the v2.0 library and "smartavoid" code with the following changes:
    1. Totally eliminated the CircuitPython SimpleIO library. Audio now uses PulseIO.PWMOut (also used for the servos). This not only eliminated a library call but also resulted in louder sound
    2. Replaced the note() syntax as follows:
        instead of referencing the notes as strings, the note names are now variables.  Thus is it now C4 instead of "C4".
        This eliminated a large amount of else and elseif statements and replaced it with a much cleaner/more readable code for notes.
    3. Also replaced the note duration if statements with a calculation (1/duration). The numbers come out mostly the same, but it's less code and gives more options (you can do 3rds now!).
    4. Similarly replaced the color definitions for the LEDs with variable names that are direct references to color tuples. So it's led[0]=red not led[0]="red".  
    5. Added two more colors: white (for 255, 255, 255), and black (0, 0, 0), which is the same as setting the led to "off".
    6. Adjusted the servo speeds to try to make the sides more even. This is really hard to get JUST right, but i think it's closer now.

--- Trinket m0/codekitty.py
+++ Trinket m0/codekitty.py
@@ -1,169 +1,116 @@
-from time import sleep
-import simpleio
 import board
-import adafruit_dotstar
-import touchio
+from time import sleep
+from adafruit_dotstar import DotStar
+from touchio import TouchIn
 from analogio import AnalogIn  
-import pulseio 
+from pulseio import PWMOut
 
-### SOUND FUNCTIONS
-def note(notestr,durint):
-    # OCTAVE 3
-    if (notestr == "C3"):
-        notefreq = 131
-    elif (notestr == "C#3"):
-        notefreq = 139
-    elif (notestr == "D3"):
-        notefreq = 147
-    elif (notestr == "Eb3"):
-        notefreq = 156
-    elif (notestr == "E3"):
-        notefreq = 165
-    elif (notestr == "F3"):
-        notefreq = 175
-    elif (notestr == "F#3"):
-        notefreq = 185
-    elif (notestr == "G3"):
-        notefreq = 196
-    elif (notestr == "G#3"):
-        notefreq = 208
-    elif (notestr == "A3"):
-        notefreq = 220
-    elif (notestr == "Bb3"):
-        notefreq = 233
-    elif (notestr == "B3"):
-        notefreq = 247
-    # OCTAVE 4
-    elif (notestr == "C4"):
-        notefreq = 262
-    elif (notestr == "C#4"):
-        notefreq = 277
-    elif (notestr == "D4"):
-        notefreq = 294
-    elif (notestr == "Eb4"):
-        notefreq = 293
-    elif (notestr == "E4"):
-        notefreq = 330
-    elif (notestr == "F4"):
-        notefreq = 349
-    elif (notestr == "F#4"):
-        notefreq = 370
-    elif (notestr == "G4"):
-        notefreq = 392
-    elif (notestr == "G#4"):
-        notefreq = 415
-    elif (notestr == "A4"):
-        notefreq = 440
-    elif (notestr == "Bb4"):
-        notefreq = 466
-    elif (notestr == "B4"):
-        notefreq = 494
-    # OCTAVE 5
-    elif (notestr == "C5"):
-        notefreq = 523
-    elif (notestr == "C#5"):
-        notefreq = 554
-    elif (notestr == "D5"):
-        notefreq = 587
-    elif (notestr == "Eb5"):
-        notefreq = 622
-    elif (notestr == "E5"):
-        notefreq = 659
-    elif (notestr == "F5"):
-        notefreq = 699
-    elif (notestr == "F#5"):
-        notefreq = 740
-    elif (notestr == "G5"):
-        notefreq = 784
-    elif (notestr == "G#5"):
-        notefreq = 831
-    elif (notestr == "A5"):
-        notefreq = 880
-    elif (notestr == "Bb5"):
-        notefreq = 932
-    elif (notestr == "B5"):
-        notefreq = 988
-    else:
-        print("Invalid note")
-        
-    if (durint == 1):
-        notedur = 1.0
-    elif (durint == 2):
-        notedur = 0.5
-    elif (durint == 4):
-        notedur = 0.25
-    elif (durint == 8):
-        notedur = 0.125
-    else:
-        print("Invalid note duration")
-        
-    if (notestr == "rest"):
-        sleep(notedur)   
+# OCTAVE 3 
+C3 = 131
+Cs3 = 139
+D3 = 147
+Eb3 = 156
+E3 = 165
+F3 = 175
+Fs3 = 185
+G3 = 196
+Gs3 = 208
+A3 = 220
+Bb3 = 233
+B3 = 247
+# OCTAVE 4
+C4 = 262
+Cs4 = 277
+D4 = 294
+Eb4 = 293
+E4 = 330
+F4 = 349
+Fs4 = 370
+G4 = 392
+Gs4 = 415
+A4 = 440
+Bb4 = 466
+B4 = 494
+# OCTAVE 5
+C5 = 523
+Cs5 = 554
+D5 = 587
+Eb5 = 622
+E5 = 659
+F5 = 699
+Fs5 = 740
+G5 = 784
+Gs5 = 831
+A5 = 880
+Bb5 = 932
+B5 = 988
 
-    simpleio.tone(board.D3, notefreq, duration=notedur)
-    sleep(notedur/2)
+
+# Setup our piezo object
+piezo = PWMOut(board.A3, duty_cycle=0, frequency=440, variable_frequency=True)
+
+
+def note(note,notedur=4):
+    duration=1/notedur
+    piezo.frequency = note
+    piezo.duty_cycle = 65536 // 2  # On 50%
+    sleep(duration)  # On for 1/4 second
+    piezo.duty_cycle = 0  # Off
+    sleep(0.05)
+    
     
 def rest(restint):
-    if (restint == 1):
-        restdur = 1.0
-    elif (restint == 2):
-        restdur = 0.5
-    elif (restint == 4):
-        restdur = 0.25
-    elif (restint == 8):
-        restdur = 0.125
-    else:
-        print("Invalid rest duration")
-    sleep(restdur)
-                
+    rest=1/restint
+    sleep(rest)
+
+    
 def march():
     # First measure
-    note("A4",2)
-    note("A4",2)
-    note("A4",2)
-    note("F4",4)
-    note("C4",4)
-    note("A4",2)
-    note("F4",4)
-    note("C4",4)
-    note("A4",1)
+    note(A4,2)
+    note(A4,2)
+    note(A4,2)
+    note(F4,4)
+    note(C4,4)
+    note(A4,2)
+    note(F4,4)
+    note(C4,4)
+    note(A4,1)
     # Second Measure
-    note("E5",2)
-    note("E5",2)
-    note("E5",2)
-    note("F5",4)
-    note("C5",4)
-    note("G#4",2)
-    note("F4",4)
-    note("C4",4)
-    note("A4",1)
-    note("A5",2)
-    note("A4",4)
-    note("A4",4)
-    note("A5",2)
-    note("G#5",4)
-    note("G5",4)
+    note(E5,2)
+    note(E5,2)
+    note(E5,2)
+    note(F5,4)
+    note(C5,4)
+    note(Gs4,2)
+    note(F4,4)
+    note(C4,4)
+    note(A4,1)
+    note(A5,2)
+    note(A4,4)
+    note(A4,4)
+    note(A5,2)
+    note(Gs5,4)
+    note(G5,4)
     # MEASURE 3
-    note("F#5",4)
-    note("F5",4)
-    note("F#5",4)
-    note("Bb4",4)
-    note("Eb4",2)
-    note("D4",4)
-    note("C#4",4)
-    note("C4",4)
-    note("B4",4)
-    note("C4",4)
-    note("F3",4)
-    note("G#3",2)
-    note("F3",4)
-    note("G#3",2)
+    note(Fs5,4)
+    note(F5,4)
+    note(Fs5,4)
+    note(Bb4,4)
+    note(Eb4,2)
+    note(D4,4)
+    note(Cs4,4)
+    note(C4,4)
+    note(B4,4)
+    note(C4,4)
+    note(F3,4)
+    note(Gs3,2)
+    note(F3,4)
+    note(Gs3,2)
 
     
 def beep():
-    simpleio.tone(board.D3, 330, duration=0.25)
-    
-simpleio.tone(board.D3, 1, duration=0)   
+    note(E3,4)
+
     
 # SERVO FUNCTIONS
 
@@ -181,27 +128,27 @@ def angle2dc(angle):
 # Stop:         1.5 ms  -- old 90   -- 4915
 # Full reverse: 1.0 ms  -- old 0
 
-leftServo = pulseio.PWMOut(board.A2, duty_cycle=4915,  frequency=50)
-rightServo = pulseio.PWMOut(board.A1, duty_cycle=4915,  frequency=50)
+leftServo = PWMOut(board.A2, duty_cycle=4915,  frequency=50)
+rightServo = PWMOut(board.A1, duty_cycle=4915,  frequency=50)
 
 def stop():
     leftServo.duty_cycle = angle2dc(90)
     rightServo.duty_cycle = angle2dc(90)
 
+    
 def go(t,spdtxt="fast"):
     if(spdtxt == 'fast'):
         leftServo.duty_cycle = angle2dc(0)
         rightServo.duty_cycle = angle2dc(180)
     elif(spdtxt == 'slow'):
-        leftServo.duty_cycle = angle2dc(50)
-        rightServo.duty_cycle = angle2dc(120)
+        leftServo.duty_cycle = angle2dc(30)
+        rightServo.duty_cycle = angle2dc(130)
     else:
         print("Invalid Argument")
     sleep(t)
     stop()
 
 
-    
 def left(turn=90):
     if(turn == 90):
         leftServo.duty_cycle = angle2dc(90)
@@ -209,11 +156,12 @@ def left(turn=90):
         sleep(.35)
     elif(turn == 45):
         leftServo.duty_cycle = angle2dc(90)
-        rightServo.duty_cycle = angle2dc(120)
+        rightServo.duty_cycle = angle2dc(144)
         sleep(.6)
     else:
         print("Invalid Argument")
     stop()
+
     
 def right(turn=90):
     if(turn == 90):
@@ -221,36 +169,40 @@ def right(turn=90):
         rightServo.duty_cycle = angle2dc(90)
         sleep(.65)
     elif(turn == 45):
-        leftServo.duty_cycle = angle2dc(60)
+        leftServo.duty_cycle = angle2dc(72)
         rightServo.duty_cycle = angle2dc(90)
         sleep(.5)
     else:
         print("Invalid Argument")
     stop()
+
     
 def back(t,spdtxt="fast"):
     if(spdtxt == 'fast'):
         leftServo.duty_cycle = angle2dc(180)
         rightServo.duty_cycle = angle2dc(0)
     elif(spdtxt == 'slow'):
-        leftServo.duty_cycle = angle2dc(120)
-        rightServo.duty_cycle = angle2dc(60)
+        leftServo.duty_cycle = angle2dc(144)
+        rightServo.duty_cycle = angle2dc(72)
     else:
         print("Invalid Argument")
     sleep(t)
     stop()
 
+
 ### TOUCH FUNCTIONS
 touch_pad = board.A0
-touch = touchio.TouchIn(touch_pad)
+touch = TouchIn(touch_pad)
 # touch.value == True if touched
+
 
 ### SENSOR FUNCTIONS
 sensor = AnalogIn(board.A4)
 # sensor.value is value of sensor input
 
+
 ### LED FUNCTIONS ###
-ledlight = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
+ledlight = DotStar(board.APA102_SCK, board.APA102_MOSI, 1)
 
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
@@ -266,26 +218,23 @@ def wheel(pos):
     return int(pos * 3), 0, int(255 - (pos * 3))
 
 
-def led(color,br=0.3):
+# Define our colors
+red=[255,0,0]
+orange=[255,165,0]
+yellow=[255,255,0]
+green=[0,255,0]
+blue=[0,0,255]
+purple=[128,0,128]
+pink=[255,192,203]
+black=[0, 0, 0]
+white=[255,255,255]
+
+
+def led(colorin,br=0.3):
     if(br>1):
         br=1
     ledlight.brightness=br
-    if(color=="red"):
-        ledlight[0]=(255,0,0)
-    elif(color=="orange"):
-        ledlight[0]=(255,165,0)
-    elif(color=="yellow"):
-        ledlight[0]=(255,255,0)
-    elif(color=="green"):
-        ledlight[0]=(0,255,0)
-    elif(color=="blue"):
-        ledlight[0]=(0,0,255)
-    elif(color=="purple"):
-        ledlight[0]=(128,0,128)
-    elif(color=="pink"):
-        ledlight[0]=(255,192,203)
-    elif(color=="off"):
-        ledlight[0]=(0, 0, 0)
+    ledlight[0]=colorin
         
 
 def rainbow():
--- Trinket m0/main.py
+++ Trinket m0/main.py
@@ -1,15 +1,60 @@
-print("Code Kitty v2.2 online")
+print("Code Kitty v2.2 Booting")
 from codekitty import *
 from time import sleep
+from random import randint
+
+print("Code Kitty v2.2 Ready!")
+beep()
+
+def randTurn():
+    td=randint(1,2)
+    if(td==1):
+        print("[CK] LEFT(90)") 
+        left(90)
+    elif(td==2):
+        print("[CK] RIGHT(90)") 
+        right(90)
+
 while True:
-    if(sensor.value < 6000):
-        led("purple")
-        beep()
-    else:
-        led("red")
-    
-    if(touch.value == True):
-        go(.2)
-        led("blue")
-        sleep(.1)
-        
+    print("[CK] STOP")
+    sleep(.5)
+    while(touch.value==False):
+        led(red)
+    note(C4,8)
+    note(E4,4)
+    note(G4,2)
+    turns=0
+    while(touch.value==False):
+        if(sensor.value<14000):
+            print("[CK] STOP")
+            stop()
+            sleep(2)
+            print("[CK] BACK")
+            led(blue)
+            back(.5,"slow")
+            sleep(2)
+            randTurn()
+            turns += 1
+            sleep(2)
+            if(turns==3):
+                led(yellow)
+                print("[CK] Frustration level 3!")
+                note(D3,2)
+                print("[CK] Getting out of here!")
+                back(1)
+                sleep(1)
+                left(90)
+                sleep(1)
+                left(90)
+                sleep(1)
+        else:
+            print("[CK] GO")
+            led(green)
+            go(.375,"slow")
+            sleep(1)
+            turns=0
+    note(E5,8)
+    note(A4,8)
+    note(C5,4)
+    note(G5,4)
+    note(A4,2)    
