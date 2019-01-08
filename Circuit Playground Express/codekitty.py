bf35ebfd46c433f67fab23b604619dda7345f217
codekitty-jason <30638615+codekitty-jason@users.noreply.github.com>
2019-01-08 13:00:14 -0600

    New version with many changes. See description
    This version of the codekitty library has several significant changes:
    1. Switched totally away from the SimpleIO library. This lowers the memory footprint and (ironically) simplifies the code
    2. Fully integrated the Adafruit_Circuitplayground.Express library. This allows a simpler library. We already reinvented the (actual) wheel, so we don't need to do it in our code too.
    3. The go() and other movement functions are totally new.  Now, instead of speed words like "Fast" and "slow" there are numbers 1-6 to set the speed. 6 is the fastest. The default setting is 4. The turn functions now use the same exact syntax as go() and back().  This means left(1) will turn left (at the default speed of 4) for one second. This allows a lot more control over turning and speed in any direction, with less double quotes.
    4. The note() function and the cpx.pixels[] both use VARIABLES instead of strings now.  The note function also uses abbreviations for timing instead of numbers.  This means a quarter note in middle C is now note(C4,qn) and to turn the 2nd neopixel LED red, you can use cpx.pixel[1] = red
    Yay for less double qoutes.
    Please see the following document for a complete list of CK and CPX functions:
    https://docs.google.com/document/d/1erGQ-NwgxcbJU-y29Z6sNWUsuOuClQFSehLuYvbhbwM/edit

--- Circuit Playground Express/codekitty.py
+++ Circuit Playground Express/codekitty.py
@@ -1,172 +1,12 @@
-from time import sleep
-import simpleio
 import board
-#import adafruit_dotstar
-import touchio
-from analogio import AnalogIn  
-import pulseio 
+from time import sleep
+from adafruit_circuitplayground.express import cpx
+from pulseio import PWMOut
 
-### SOUND FUNCTIONS
-def note(notestr,durint):
-    # OCTAVE 3
-    if (notestr == "C3"):
-        notefreq = 131
-    elif (notestr == "C#3"):
-        notefreq = 139
-    elif (notestr == "D3"):
-        notefreq = 147
-    elif (notestr == "Eb3"):
-        notefreq = 156
-    elif (notestr == "E3"):
-        notefreq = 165
-    elif (notestr == "F3"):
-        notefreq = 175
-    elif (notestr == "F#3"):
-        notefreq = 185
-    elif (notestr == "G3"):
-        notefreq = 196
-    elif (notestr == "G#3"):
-        notefreq = 208
-    elif (notestr == "A3"):
-        notefreq = 220
-    elif (notestr == "Bb3"):
-        notefreq = 233
-    elif (notestr == "B3"):
-        notefreq = 247
-    # OCTAVE 4
-    elif (notestr == "C4"):
-        notefreq = 262
-    elif (notestr == "C#4"):
-        notefreq = 277
-    elif (notestr == "D4"):
-        notefreq = 294
-    elif (notestr == "Eb4"):
-        notefreq = 293
-    elif (notestr == "E4"):
-        notefreq = 330
-    elif (notestr == "F4"):
-        notefreq = 349
-    elif (notestr == "F#4"):
-        notefreq = 370
-    elif (notestr == "G4"):
-        notefreq = 392
-    elif (notestr == "G#4"):
-        notefreq = 415
-    elif (notestr == "A4"):
-        notefreq = 440
-    elif (notestr == "Bb4"):
-        notefreq = 466
-    elif (notestr == "B4"):
-        notefreq = 494
-    # OCTAVE 5
-    elif (notestr == "C5"):
-        notefreq = 523
-    elif (notestr == "C#5"):
-        notefreq = 554
-    elif (notestr == "D5"):
-        notefreq = 587
-    elif (notestr == "Eb5"):
-        notefreq = 622
-    elif (notestr == "E5"):
-        notefreq = 659
-    elif (notestr == "F5"):
-        notefreq = 699
-    elif (notestr == "F#5"):
-        notefreq = 740
-    elif (notestr == "G5"):
-        notefreq = 784
-    elif (notestr == "G#5"):
-        notefreq = 831
-    elif (notestr == "A5"):
-        notefreq = 880
-    elif (notestr == "Bb5"):
-        notefreq = 932
-    elif (notestr == "B5"):
-        notefreq = 988
-    else:
-        print("Invalid note")
-        
-    if (durint == 1):
-        notedur = 1.0
-    elif (durint == 2):
-        notedur = 0.5
-    elif (durint == 4):
-        notedur = 0.25
-    elif (durint == 8):
-        notedur = 0.125
-    else:
-        print("Invalid note duration")
-        
-    if (notestr == "rest"):
-        sleep(notedur)   
 
-    simpleio.tone(board.D3, notefreq, duration=notedur)
-    sleep(notedur/2)
-    
-def rest(restint):
-    if (restint == 1):
-        restdur = 1.0
-    elif (restint == 2):
-        restdur = 0.5
-    elif (restint == 4):
-        restdur = 0.25
-    elif (restint == 8):
-        restdur = 0.125
-    else:
-        print("Invalid rest duration")
-    sleep(restdur)
-                
-def march():
-    # First measure
-    note("A4",2)
-    note("A4",2)
-    note("A4",2)
-    note("F4",4)
-    note("C4",4)
-    note("A4",2)
-    note("F4",4)
-    note("C4",4)
-    note("A4",1)
-    # Second Measure
-    note("E5",2)
-    note("E5",2)
-    note("E5",2)
-    note("F5",4)
-    note("C5",4)
-    note("G#4",2)
-    note("F4",4)
-    note("C4",4)
-    note("A4",1)
-    note("A5",2)
-    note("A4",4)
-    note("A4",4)
-    note("A5",2)
-    note("G#5",4)
-    note("G5",4)
-    # MEASURE 3
-    note("F#5",4)
-    note("F5",4)
-    note("F#5",4)
-    note("Bb4",4)
-    note("Eb4",2)
-    note("D4",4)
-    note("C#4",4)
-    note("C4",4)
-    note("B4",4)
-    note("C4",4)
-    note("F3",4)
-    note("G#3",2)
-    note("F3",4)
-    note("G#3",2)
-
-    
-def beep():
-    simpleio.tone(board.A0, 330, duration=0.25)
-    
-simpleio.tone(board.A0, 1, duration=0)   
-    
+#    
 # SERVO FUNCTIONS
-
+#
 def ms2dc(ms):
     # convert ms to 16 bit duty cycle
     return int(65535 * ms / 20)
@@ -176,77 +16,189 @@ def angle2dc(angle):
     ms = 1 + (angle/180)
     return int(65535 * ms / 20)
 
-
-def unit2dc(speed):
-    # convert -1 to 1 to 16 bit duty cycle
-    ms = (1 + speed) * 1000 
-    return int(65535 * ms / 20)
-
-
 # Servo runs at 50Hz with the following duty cycle times:
 # Full forward: 2.0 ms  -- old 180
 # Stop:         1.5 ms  -- old 90   -- 4915
 # Full reverse: 1.0 ms  -- old 0
 
-leftServo = pulseio.PWMOut(board.A3, frequency=50)
-rightServo = pulseio.PWMOut(board.A6, frequency=50)
 
-def throttle(speed):
-    leftServo.duty_cycle = unit2dc(speed)
-    rightServo.duty_cycle = unit2dc(speed)
+leftServo = PWMOut(board.A3, duty_cycle=4915,  frequency=50)
+rightServo = PWMOut(board.A6, duty_cycle=4915,  frequency=50)
+
 
 def stop():
     leftServo.duty_cycle = angle2dc(90)
     rightServo.duty_cycle = angle2dc(90)
 
-def go(t,spdtxt="fast"):
-    if(spdtxt == 'fast'):
-        leftServo.duty_cycle = angle2dc(180)
-        rightServo.duty_cycle = angle2dc(180)
-    elif(spdtxt == 'slow'):
-        leftServo.duty_cycle = angle2dc(140)
-        rightServo.duty_cycle = angle2dc(140)
+    
+def go(t,spd=5):
+    if spd in range(1,7):
+        cspd=(spd*10)+120
+        leftServo.duty_cycle = angle2dc(cspd)
+        rightServo.duty_cycle = angle2dc(cspd)
     else:
-        print("Invalid Argument")
+        print("[ckSpeed must be between 1 and 6")
     sleep(t)
     stop()
 
 
-    
-def left(turn=90):
-    if(turn == 90):
+def left(t,spd=5):
+    if spd in range(1,7):
+        cspd=(spd*10)+120
         leftServo.duty_cycle = angle2dc(90)
-        rightServo.duty_cycle = angle2dc(180)
-        sleep(.35)
-    elif(turn == 45):
-        leftServo.duty_cycle = angle2dc(90)
-        rightServo.duty_cycle = angle2dc(140)
-        sleep(.6)
+        rightServo.duty_cycle = angle2dc(cspd)
     else:
-        print("Invalid Argument")
-    stop()
-    
-def right(turn=90):
-    if(turn == 90):
-        leftServo.duty_cycle = angle2dc(180)
-        rightServo.duty_cycle = angle2dc(90)
-        sleep(.65)
-    elif(turn == 45):
-        leftServo.duty_cycle = angle2dc(140)
-        rightServo.duty_cycle = angle2dc(90)
-        sleep(.5)
-    else:
-        print("Invalid Argument")
-    stop()
-    
-def back(t,spdtxt="fast"):
-    if(spdtxt == 'fast'):
-        leftServo.duty_cycle = angle2dc(0)
-        rightServo.duty_cycle = angle2dc(0)
-    elif(spdtxt == 'slow'):
-        leftServo.duty_cycle = angle2dc(50)
-        rightServo.duty_cycle = angle2dc(50)
-    else:
-        print("Invalid Argument")
+        print("[ckSpeed must be between 1 and 6")
     sleep(t)
     stop()
+
+    
+def right(t,spd=5):
+    if spd in range(1,7):
+        cspd=(spd*10)+120
+        leftServo.duty_cycle = angle2dc(cspd)
+        rightServo.duty_cycle = angle2dc(90)
+    else:
+        print("[ckSpeed must be between 1 and 6")
+    sleep(t)
+    stop()
+
+    
+def back(t,spd=5):
+    if spd in range(1,7):
+        cspd=70-(spd*10)
+        leftServo.duty_cycle = angle2dc(cspd)
+        rightServo.duty_cycle = angle2dc(cspd)
+    else:
+        print("[ckSpeed must be between 1 and 6")
+    sleep(t)
+    stop()
+    
+###
+### Sound Functions
+###
+def meow():
+    cpx.play_file("Meow.wav")
+    
+    
+def purr():
+    cpx.play_file("Purr.wav")
+
+
+# Setup the note frequencies    
+# OCTAVE 3 
+C3 = 131
+Cs3 = 139
+D3 = 147
+Eb3 = 156
+E3 = 165
+F3 = 175
+Fs3 = 185
+G3 = 196
+Gs3 = 208
+A3 = 220
+Bb3 = 233
+B3 = 247
+# OCTAVE 4
+C4 = 262
+Cs4 = 277
+D4 = 294
+Eb4 = 293
+E4 = 330
+F4 = 349
+Fs4 = 370
+G4 = 392
+Gs4 = 415
+A4 = 440
+Bb4 = 466
+B4 = 494
+# OCTAVE 5
+C5 = 523
+Cs5 = 554
+D5 = 587
+Eb5 = 622
+E5 = 659
+F5 = 699
+Fs5 = 740
+G5 = 784
+Gs5 = 831
+A5 = 880
+Bb5 = 932
+B5 = 988
+
+# set up time signature
+wn = 1.5  # adjust this to change tempo of everything
+# these notes are fractions of the whole note
+hn = wn / 2
+qn = wn / 4
+dqn = qn * 1.5
+en = wn / 8
+
+
+def note(freq,dur):
+    cpx.play_tone(freq,dur)
+
+  
+def march():
+    # MEASURE 1
+    note(A4,2)
+    note(A4,2)
+    note(A4,2)
+    note(F4,4)
+    note(C4,4)
+    note(A4,2)
+    note(F4,4)
+    note(C4,4)
+    note(A4,1)
+    # MEASURE 2
+    note(E5,2)
+    note(E5,2)
+    note(E5,2)
+    note(F5,4)
+    note(C5,4)
+    note(Gs4,2)
+    note(F4,4)
+    note(C4,4)
+    note(A4,1)
+    note(A5,2)
+    note(A4,4)
+    note(A4,4)
+    note(A5,2)
+    note(Gs5,4)
+    note(G5,4)
+    # MEASURE 3
+    note(Fs5,4)
+    note(F5,4)
+    note(Fs5,4)
+    note(Bb4,4)
+    note(Eb4,2)
+    note(D4,4)
+    note(Cs4,4)
+    note(C4,4)
+    note(B4,4)
+    note(C4,4)
+    note(F3,4)
+    note(Gs3,2)
+    note(F3,4)
+    note(Gs3,2)
+
+    
+def beep():
+    note(A4,4)
+
+    
+###
+### Neopixel functions
+###
+
+# Setup the colors
+red=(255, 0, 0)
+orange=(255, 165, 0)
+yellow=(255, 255, 0)
+green=(0, 255, 0)
+blue=(0, 0, 255)
+pink=(255, 192, 203)
+purple=(238, 130, 238)
+white=(255, 255, 255)
+black=(0, 0, 0)
+
